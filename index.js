831cffa25385eb606396d5214ef061fc84042e0b
nhan6104 <thanhan.nguyen6104@gmail.com>
2024-03-02 15:51:40 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ var phoneNumber = "0981430418";
 var email = "minh.luxuanhcmut.edu.vn";
 var otp = "8647";
 var usersAuthenticate = new tdlogistics_1.UsersAuthenticate();
+var partnerStaff = new tdlogistics_1.PartnerStaffOperation();
+var businessOperation = new tdlogistics_1.BusinessOperation();
+var staffOperation = new tdlogistics_1.StaffOperation();
+var partnerInfo = {
+    partner_id: "26546556",
+    username: "nhantd",
+    password: "12355648979",
+    fullname: "Nguyễn Thành Nhân",
+    email: "thanhan.nguyen@gmail.com",
+    phone_number: "090807964604",
+    date_of_birth: "06-01-2004",
+    cccd: "079206589515",
+    province: "string",
+    district: "string",
+    town: "string",
+    detail_address: "string",
+    role: "string",
+    position: "string",
+    bin: "string",
+    bank: "string",
+};
 usersAuthenticate.verifyOTP(phoneNumber, email, otp)
     .then(function (result) { return console.log(result); })
     .catch(function (error) { return console.log(error); });
@@ -14,3 +35,7 @@ var password = "NTDung@tdlogistics2k24";
 staffsAuthenticate.login(username, password)
     .then(function (result) { return console.log(result); })
     .catch(function (error) { return console.log(error); });
+partnerStaff.create(partnerInfo)
+    .then(function (result) { return console.log(result); })
+    .catch(function (error) { return console.log(error); });
+;
--- index.ts
+++ index.ts
@@ -1,10 +1,37 @@
-import { UsersAuthenticate , StaffsAuthenticate, UsersOperation } from "./tdlogistics";
+import { UsersAuthenticate , StaffsAuthenticate, UsersOperation, BusinessOperation, StaffOperation, PartnerStaffOperation } from "./tdlogistics";
 
 const phoneNumber = "0981430418";
 const email = "minh.luxuanhcmut.edu.vn";
 const otp = "8647";
 
 const usersAuthenticate = new UsersAuthenticate();
+const partnerStaff = new PartnerStaffOperation();
+const businessOperation = new BusinessOperation();
+const staffOperation = new StaffOperation(); 
+
+const partnerInfo = {
+  partner_id: "26546556",
+  username: "nhantd",
+  password: "12355648979",
+  fullname: "Nguyễn Thành Nhân",
+  email: "thanhan.nguyen@gmail.com",
+  phone_number: "090807964604",
+  date_of_birth: "06-01-2004", 
+  cccd: "079206589515",
+  province: "string",
+  district: "string",
+  town: "string",
+  detail_address: "string",
+  role: "string",
+  position: "string",
+  bin: "string",
+  bank: "string",
+}
+
+
+
+
+
 usersAuthenticate.verifyOTP(phoneNumber, email, otp)
 .then(result => console.log(result))
 .catch(error => console.log(error));
@@ -17,3 +44,7 @@ const password = "NTDung@tdlogistics2k24";
 staffsAuthenticate.login(username, password)
 .then(result => console.log(result))
 .catch(error => console.log(error));
+
+partnerStaff.create(partnerInfo)
+.then(result => console.log(result))
+.catch(error => console.log(error));;
