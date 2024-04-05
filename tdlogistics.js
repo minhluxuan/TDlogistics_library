a435b034c5e7e34def15bae294c7fe6d77b9c6f5
minhluxuan <minh.luxuanhcmut@hcmut.edu.vn>
2024-04-05 11:24:06 +0700

    fix bug

--- tdlogistics.js
+++ tdlogistics.js
@@ -587,7 +587,7 @@ var AgencyOperation = /** @class */ (function () {
                         for (i = 0; i < info.licenseFiles.length; i++) {
                             formData.append('files', info.licenseFiles[i]);
                         }
-                        return [4 /*yield*/, axios_1.default.put("".concat(this.baseUrl, "/update_agency_company_license?agency_id=").concat(condition.agency_id), formData, {
+                        return [4 /*yield*/, axios_1.default.post("".concat(this.baseUrl, "/update_agency_company_license?agency_id=").concat(condition.agency_id), formData, {
                                 withCredentials: true,
                             })];
                     case 1:
--- tdlogistics.ts
+++ tdlogistics.ts
@@ -313,7 +313,7 @@ export interface CreatingAgencyInfo {
     user_bin: string,
     user_bank: string,
     user_salary: number,
-    individual: boolean,
+    individual_company: boolean,
     
     tax_number?: string,
     type: string,
@@ -484,7 +484,7 @@ class AgencyOperation {
                 formData.append('files', info.licenseFiles[i]);
             }
 			// Gửi yêu cầu POST để tải lên hình ảnh
-			const response: AxiosResponse = await axios.put(`${this.baseUrl}/update_agency_company_license?agency_id=${condition.agency_id}`, formData , {
+			const response: AxiosResponse = await axios.post(`${this.baseUrl}/update_agency_company_license?agency_id=${condition.agency_id}`, formData , {
 				withCredentials: true,
 			});
 		
